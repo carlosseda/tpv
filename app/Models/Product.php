2c8da9680cf8e3fc98f3bdb6d794a0e1cd574b88
carlosseda <carlossedagambin@gmail.com>
2022-06-08 20:14:46 +0200

    Estructura de web

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,9 +11,9 @@ class Product extends Connection {
 
     public function filterCategory($category_id) {
 
-        $query =  "SELECT * FROM productos WHERE activo = 1 AND visible = 1 AND categoria_id = %s";
+        $query =  "SELECT * FROM productos WHERE activo = 1 AND visible = 1 AND categoria_id = :category_id";
                 
-        $stmt = $this->pdo->prepare($query, $category_id);
+        $stmt = $this->pdo->prepare($query);
         $result = $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
