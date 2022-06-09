98e9148e684c097e9696a3e4a601106a212af737
carlosseda <carlossedagambin@gmail.com>
2022-06-09 15:36:15 +0200

    estructura web

--- tickets.php
+++ tickets.php
@@ -35,79 +35,76 @@
             <?php endif; ?>
         </ul>
 
-        <?php if($total_ticket != null): ?>
-
-            <div class="row mt-3">
-                <div class="col">
-                    <div class="bg-secondary">
-                        <div class="row justify-content-between g-0">
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 pt-1">B. Imponible</h5>
-                            </div>
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 border-start pt-1">IVA</h5>
-                            </div>
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 bg-dark pt-1">TOTAL</h5>
-                            </div>
+        <div class="row mt-3">
+            <div class="col">
+                <div class="bg-secondary">
+                    <div class="row justify-content-between g-0">
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 pt-1">B. Imponible</h5>
                         </div>
-                        <div class="row justify-content-between g-0">
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 pb-1"><?= $total_ticket['base_imponible']; ?></h5>
-                            </div>
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 border-start pb-1">21%</h5>
-                            </div>
-                            <div class="col">
-                                <h5 class="text-center text-white mb-0 bg-dark pb-1"><?= $total_ticket['total']; ?></h5>
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 border-start pt-1">IVA</h5>
+                        </div>
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 bg-dark pt-1">TOTAL</h5>
+                        </div>
+                    </div>
+                    <div class="row justify-content-between g-0">
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 pb-1"><?= $total_ticket['base_imponible']; ?></h5>
+                        </div>
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 border-start pb-1">21%</h5>
+                        </div>
+                        <div class="col">
+                            <h5 class="text-center text-white mb-0 bg-dark pb-1"><?= $total_ticket['total']; ?></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-3 mb-3">
+            <div class="col-6">
+                <div><a class="btn btn-danger btn-lg w-100" role="button" href="#myModal" data-bs-toggle="modal">ELIMINAR</a>
+                    <div class="modal fade" role="dialog" tabindex="-1" id="myModal">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4>Eliminar ticket</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <p class="text-center text-muted">Está a punto de borrar el pedido sin ser cobrado. ¿Está completamente seguro de realizar esta acción?</p>
+                                </div>
+                                <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">CERRAR</button><button class="btn btn-success" type="button">ELIMINAR</button></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-            <div class="row mt-3 mb-3">
-                <div class="col-6">
-                    <div><a class="btn btn-danger btn-lg w-100" role="button" href="#myModal" data-bs-toggle="modal">ELIMINAR</a>
-                        <div class="modal fade" role="dialog" tabindex="-1" id="myModal">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4>Eliminar ticket</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <p class="text-center text-muted">Está a punto de borrar el pedido sin ser cobrado. ¿Está completamente seguro de realizar esta acción?</p>
-                                    </div>
-                                    <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">CERRAR</button><button class="btn btn-success" type="button">ELIMINAR</button></div>
+            <div class="col-6">
+                <div><a class="btn btn-success btn-lg w-100" role="button" href="#myModal-2" data-bs-toggle="modal">COBRAR</a>
+                    <div class="modal fade" role="dialog" tabindex="-1" id="myModal-2">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h4>Metodo de pago</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div><a class="btn btn-success btn-lg w-100" role="button" href="#myModal-2" data-bs-toggle="modal">COBRAR</a>
-                        <div class="modal fade" role="dialog" tabindex="-1" id="myModal-2">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h4>Metodo de pago</h4><button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                <div class="modal-body">
+                                    <div class="row align-items-center flex-column">
+                                        <div class="col-6 d-lg-flex m-2"><button class="btn btn-primary w-100" type="button">EFECTIVO</button></div>
+                                        <div class="col-6 d-lg-flex m-2"><button class="btn btn-success w-100" type="button">TARJETA CRÉDITO</button></div>
+                                        <div class="col-6 d-lg-flex m-2"><button class="btn btn-danger w-100" type="button">BIZUM</button></div>
                                     </div>
-                                    <div class="modal-body">
-                                        <div class="row align-items-center flex-column">
-                                            <div class="col-6 d-lg-flex m-2"><button class="btn btn-primary w-100" type="button">EFECTIVO</button></div>
-                                            <div class="col-6 d-lg-flex m-2"><button class="btn btn-success w-100" type="button">TARJETA CRÉDITO</button></div>
-                                            <div class="col-6 d-lg-flex m-2"><button class="btn btn-danger w-100" type="button">BIZUM</button></div>
-                                        </div>
-                                    </div>
-                                    <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">CERRAR</button></div>
                                 </div>
+                                <div class="modal-footer"><button class="btn btn-light" type="button" data-bs-dismiss="modal">CERRAR</button></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
-        <?php endif; ?>
 
     </aside>
 </div>
