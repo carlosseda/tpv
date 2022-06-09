82ff831be439427c161ae5508a6a2b4d2e44f03a
carlosseda <carlossedagambin@gmail.com>
2022-06-09 16:50:22 +0200

    estructura

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -27,7 +27,7 @@ class Ticket extends Connection {
 
     public function total($table_id) {
 
-        $query =  "SELECT ROUND(SUM(precios.precio_base),2) AS base_imponible, ROUND(SUM(precios.precio_base * ivas.multiplicador), 2) AS total
+        $query =  "SELECT SUM(precios.precio_base) AS base_imponible, SUM(precios.precio_base * ivas.multiplicador) AS total, 
         FROM tickets 
         INNER JOIN precios ON tickets.precio_id = precios.id 
         INNER JOIN ivas ON precios.iva_id = ivas.id
