b26fac4cf7484f6468f79d4faaeaab9ac1cd4f0f
carlosseda <carlossedagambin@gmail.com>
2022-06-09 13:02:23 +0200

    estructura web

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -11,10 +11,10 @@ class Ticket extends Connection {
 
     public function show($table_id) {
 
-        $query =  "SELECT productos.nombre AS nombre, precios.precio_base AS precio_base, productos.imagen_url AS imagen_url, categorias.nombre AS categoria FROM tickets 
+        $query =  "SELECT productos.nombre AS nombre, precios.precio_base AS precio_base, productos.imagen_url AS imagen_url, productos_categorias.nombre AS categoria FROM tickets 
         INNER JOIN precios ON tickets.precio_id = precios.id 
         INNER JOIN productos ON precios.producto_id = productos.id 
-        INNER JOIN categorias ON productos.categoria_id = categorias.id
+        INNER JOIN productos_categorias ON productos.categoria_id = productos_categorias.id
         WHERE tickets.activo = 1 AND tickets.venta_id = null AND tickets.mesa_id = '".$mesa_id."'";
                 
         $stmt = $this->pdo->prepare($query);
