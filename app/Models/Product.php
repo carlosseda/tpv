635a40f0dc46cfcc23aa20dffed728d25cf1a889
carlosseda <carlossedagambin@gmail.com>
2022-06-09 12:24:04 +0200

    estructura web

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,9 +11,7 @@ class Product extends Connection {
 
     public function filterCategory($category_id) {
 
-        $query =  "SELECT nombre, productos.imagen_url, produtos_categorias.nombre AS categoria 
-        FROM productos INNER JOIN productos_categorias ON productos.categoria_id = productos_categorias.id  
-        WHERE activo = 1 AND visible = 1 AND categoria_id = '".$category_id."'";
+        $query =  "SELECT * FROM productos WHERE activo = 1 AND visible = 1 AND categoria_id = '".$category_id."'";
                 
         $stmt = $this->pdo->prepare($query);
         $result = $stmt->execute();
---
+++ app/Models/Ticket.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace app\Models;
+
+require_once 'core/Connection.php';
+
+use PDO;
+use core\Connection;
+
+class Ticket extends Connection {
+
+    public function show($mesa_id) {
+
+        $query =  "SELECT productos.nombre AS nombre, precios.precio_base AS precio_base, productos.imagen_url AS imagen_url, categorias.nombre AS categoria FROM tickets 
+        INNER JOIN precios ON tickets.precio_id = precios.id 
+        INNER JOIN productos ON precios.producto_id = productos.id 
+        INNER JOIN categorias ON productos.categoria_id = categorias.id
+        WHERE tickets.activo = 1 AND tickets.venta_id = null AND tickets.mesa_id = '".$mesa_id."'";
+                
+        $stmt = $this->pdo->prepare($query);
+        $result = $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+?>
